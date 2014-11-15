f991d7646a9f89c8eec07b0c38698809908e103a
jcchen <junchangchen@gmail.com>
2014-11-15 22:45:30 +0800

    udpate exclude_files.js

--- after_prepare/exclude_files.js
+++ after_prepare/exclude_files.js
@@ -10,19 +10,31 @@ var path = require( "path" ),
     fs = require( "fs" ),
     shell = require( "shelljs" ),
     rootdir = process.argv[ 2 ],
-    config = require(rootdir + "/.cordova/config.json"),
-    iconroot = rootdir + "/assets/icon/android",
-    screenroot = rootdir + "/assets/screen/android",
-    androidroot = rootdir + "/platforms/android";
+    config = require(rootdir + "/.cordova/config.json");
+    // iconroot = rootdir + "/assets/icon/android",
+    // screenroot = rootdir + "/assets/screen/android",
+    // androidroot = rootdir + "/platforms/android";
 
-var assetwww = androidroot + "/assets/www";
+var platform_roots = ['ios', 'android'].map(function(p) {
+    return rootdir + "/platforms/" + p;
+});
 
 console.log('-running-hook: exclude_files');
 
-exclude_files.forEach(function(dir) {
-    var d = assetwww + "/" + dir;
-    shell.rm('-rf', d);
-    console.log(' [exclude_files] Remove ' + d);
+platform_roots.forEach(function(proot) {
+    try {
+        fs.lstatSync(proot).isDirectory();
+    } catch(e) {
+        console.log(" [exclude_files] " + proot + " does not exist. skip.");
+        return;
+    }
+
+    exclude_files.forEach(function(dir) {
+        var d = proot + "/assets/www/" + dir;
+        shell.rm('-rf', d);
+        console.log(' [exclude_files] Remove ' + d);
+    });
+
 });
 
 process.exit(0);
